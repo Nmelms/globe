a6e578cb101d574edd70eac594b29db1cedd7d8a
Nick Melms <nick@craftedagency.com>
2023-05-27 11:44:54 -0400

    styled slider btns

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -1,7 +1,12 @@
 import { useRef, useState } from "react"
 import { gsap } from "gsap"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function CardSlider({ images }) {
+  library.add(fas)
+
   gsap.registerPlugin(Flip)
   const myDivRefs = useRef([])
   const [num, setNum] = useState(0)
@@ -15,10 +20,10 @@ export default function CardSlider({ images }) {
     setNum(num + 1)
     console.log(num)
     myDivRefs.current[num].classList.add("full-screen")
-    Flip.from(state, { duration: 2, ease: "power1.out" })
-    Flip.from(state2, { duration: 2 })
-    Flip.from(state3, { duration: 2 })
-    Flip.from(state4, { duration: 2 })
+    Flip.from(state, { duration: 0.5 })
+    Flip.from(state2, { duration: 0.5, ease: "back.out(1)", delay: 0.005 })
+    Flip.from(state3, { duration: 0.5, ease: "back.out(1)", delay: 0.05 })
+    Flip.from(state4, { duration: 0.5, ease: "back.out(1)", delay: 0.1 })
   }
   const handlePrevClick = () => {
     let state = Flip.getState(myDivRefs.current[num])
@@ -29,10 +34,10 @@ export default function CardSlider({ images }) {
     setNum(num - 1)
 
     myDivRefs.current[num].classList.remove("full-screen")
-    Flip.from(state, { duration: 2, ease: "power1.out" })
-    Flip.from(state2, { duration: 2 })
-    Flip.from(state3, { duration: 2 })
-    Flip.from(state4, { duration: 2 })
+    Flip.from(state, { duration: 0.5 })
+    Flip.from(state2, { duration: 0.5 })
+    Flip.from(state3, { duration: 0.5 })
+    Flip.from(state4, { duration: 0.5 })
   }
 
   return (
@@ -47,9 +52,25 @@ export default function CardSlider({ images }) {
         ))}
       </div>
 
-      <div className="buttons">
-        <button onClick={() => handleClick()}>next</button>
-        <button onClick={() => handlePrevClick()}>prev</button>
+      <div className="buttons d-flex">
+        <button
+          className="slider-btn d-flex align-items-center justify-content-center"
+          onClick={() => handlePrevClick()}
+        >
+          <FontAwesomeIcon
+            className="text-white"
+            icon="fa-solid fa-chevron-left"
+          />
+        </button>
+        <button
+          className="slider-btn d-flex align-items-center justify-content-center"
+          onClick={() => handleClick()}
+        >
+          <FontAwesomeIcon
+            className="text-white"
+            icon="fa-solid fa-chevron-right"
+          />
+        </button>
       </div>
     </>
   )
