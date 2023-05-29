3a0c44a79ae808bbba2013cd2456b52f68546658
Nick Melms <nick@craftedagency.com>
2023-05-29 13:43:55 -0400

    updated font and added styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,11 @@ import "./App.css"
 
 function App() {
   let imgArr = [
-    { url: "src/assets/cappadocia-2000w.webp" },
-    { url: "src/assets/japan-alps-2000w.webp" },
+    { url: "src/assets/cappadocia-2000w.webp", title: "NORTH CAROLINA" },
+    { url: "src/assets/japan-alps-2000w.webp", title: "SOUTH CAROLINA" },
     { url: "src/assets/los-lances-2000w.webp" },
     { url: "src/assets/morocco-2000w.webp" },
+    { url: "src/assets/mushroom.jpeg", title: "AMANITA MUSCARIA" },
   ]
   const [images, setImages] = useState(imgArr)
 
--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -16,10 +16,10 @@ export default function CardSlider({ images }) {
     let state2 = Flip.getState(myDivRefs.current[num + 1])
     let state3 = Flip.getState(myDivRefs.current[num + 2])
     let state4 = Flip.getState(myDivRefs.current[num + 3])
-
     setNum(num + 1)
-    console.log(num)
+
     myDivRefs.current[num].classList.add("full-screen")
+
     Flip.from(state, { duration: 0.5 })
     Flip.from(state2, { duration: 0.5, ease: "back.out(1)", delay: 0.005 })
     Flip.from(state3, { duration: 0.5, ease: "back.out(1)", delay: 0.05 })
@@ -48,13 +48,20 @@ export default function CardSlider({ images }) {
             ref={(element) => (myDivRefs.current[index] = element)}
             style={{ backgroundImage: `url(${image.url})` }}
             className="image-card"
-          ></div>
+          >
+            <div className="title w-50 ps-4">{image.title}</div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Repellendus soluta vel, doloremque qui, ipsum blanditiis libero
+              eius corporis rem eveniet excepturi necessitatibus iste.
+            </p>
+          </div>
         ))}
       </div>
 
       <div className="buttons d-flex">
         <button
-          className="slider-btn d-flex align-items-center justify-content-center"
+          className="slider-btn d-flex align-items-center justify-content-center mx-4"
           onClick={() => handlePrevClick()}
         >
           <FontAwesomeIcon
