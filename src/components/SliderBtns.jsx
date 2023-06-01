a838c83d901d475e06ca856e843c037e06034bf7
Nick Melms <nick@craftedagency.com>
2023-05-31 20:35:51 -0400

    animates forward

--- src/components/SliderBtns.jsx
+++ src/components/SliderBtns.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { gsap } from "gsap"
 
@@ -6,34 +6,39 @@ export default function SliderBtns({ myDivRefs, num, setNum }) {
   gsap.registerPlugin(Flip)
 
   const handleClick = () => {
-    console.log("click")
-    let state = Flip.getState(myDivRefs.current[num])
-    let state2 = Flip.getState(myDivRefs.current[num + 1])
-    let state3 = Flip.getState(myDivRefs.current[num + 2])
-    let state4 = Flip.getState(myDivRefs.current[num + 3])
-    setNum(num + 1)
+    const updatedNum = num + 1
+    setNum(updatedNum)
+
+    const state = Flip.getState(myDivRefs.current[num])
+    const state2 = Flip.getState(myDivRefs.current[num + 1])
+    const state3 = Flip.getState(myDivRefs.current[num + 2])
+    const state4 = Flip.getState(myDivRefs.current[num + 3])
 
     myDivRefs.current[num].classList.add("full-screen")
 
-    Flip.from(state, { duration: 2 })
-    Flip.from(state2, { duration: 2, ease: "back.out(1)", delay: 0.005 })
-    Flip.from(state3, { duration: 2, ease: "back.out(1)", delay: 0.05 })
-    Flip.from(state4, { duration: 2, ease: "back.out(1)", delay: 0.1 })
+    Flip.from(state, { duration: 10 })
+    Flip.from(state2, { duration: 10, ease: "back.out(1)", delay: 0.005 })
+    Flip.from(state10, { duration: 10, ease: "back.out(1)", delay: 0.05 })
+    Flip.from(state4, { duration: 10, ease: "back.out(1)", delay: 0.1 })
   }
+
   const handlePrevClick = () => {
-    let state = Flip.getState(myDivRefs.current[num])
-    let state2 = Flip.getState(myDivRefs.current[num + 1])
-    let state3 = Flip.getState(myDivRefs.current[num + 2])
-    let state4 = Flip.getState(myDivRefs.current[num + 3])
+    const updatedNum = num - 1
+    setNum(updatedNum)
 
-    setNum(num - 1)
+    const state = Flip.getState(myDivRefs.current[updatedNum])
+    const state2 = Flip.getState(myDivRefs.current[updatedNum + 1])
+    const state3 = Flip.getState(myDivRefs.current[updatedNum + 2])
+    const state4 = Flip.getState(myDivRefs.current[updatedNum + 3])
 
-    myDivRefs.current[num].classList.remove("full-screen")
-    Flip.from(state, { duration: 2 })
-    Flip.from(state2, { duration: 2 })
-    Flip.from(state3, { duration: 2 })
-    Flip.from(state4, { duration: 2 })
+    myDivRefs.current[updatedNum].classList.remove("full-screen")
+
+    Flip.from(state, { duration: 10 })
+    Flip.from(state2, { duration: 10 })
+    Flip.from(state3, { duration: 10 })
+    Flip.from(state4, { duration: 10 })
   }
+
   return (
     <div className="buttons d-flex col-4 align-items-center">
       <button
